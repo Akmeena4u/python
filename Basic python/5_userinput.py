cd7866c9ba6d4b422979917f2bc32f6c17836af0
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-28 22:36:10 +0530

    Create 5_userinput.py

---
+++ Basic python/5_userinput.py
@@ -0,0 +1,33 @@
+english_marks = int(input('Enter marks in English\n'))  #input always returns string value
+maths_marks = int(input('Enter marks in Maths\n'))
+science_marks = int(input('Enter marks in Science\n'))
+
+average_marks = (english_marks + science_marks + maths_marks) / 3
+
+print('average marks are', average_marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = int(input('Enter a\n'))
+# b = int(input('Enter b\n'))
+
+# # print(type(a))
+# # print(type(b))
+
+# c = a*b
+
+# print(c)
+
