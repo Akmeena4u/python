9dc2c3081d3160c9016093b6de6f8661fd1f26fa
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 10:50:10 +0530

    Create 13_functions.py

---
+++ Basic python/13_functions.py
@@ -0,0 +1,104 @@
+
+/*
+#A function is a block of code which only runs when it is called.
+
+#You can pass data, known as parameters, into a function.
+
+#A function can return data as a result.
+
+#If the number of arguments is unknown, add a * before the parameter name:
+
+#In Python a function is defined using the def keyword:
+
+
+def greetings(names):
+	for name in names:
+		print('Hello', name)
+
+
+names = ['Anuj', 'Shivam', 'Rohits']
+greetings(names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sum_of_list(a):
+# 	print('Calculating...')
+# 	return sum(a)
+
+# marks = [45, 16, 87, 98, 45]
+# sum_of_marks = sum_of_list(marks)
+
+# print('my sum of marks', sum_of_marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def greet(name, dish="Pasta"):
+# 	print('Good Morning,', name)
+# 	print('Please eat', dish)
+
+
+# greet('Anuj', 'Samosa')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from math import *
+
+# import my_module as mm
+
+# print(mm.average(4, 5))
