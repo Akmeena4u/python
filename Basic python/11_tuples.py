3a65be543acdc857b1f2e8b48dfe3ea4a610953b
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 10:36:31 +0530

    Create 11_tuples.py

---
+++ Basic python/11_tuples.py
@@ -0,0 +1,21 @@
+/*
+Python Tuple is a collection of objects separated by commas. In some ways, a tuple is similar to a list in terms of indexing, nested objects, and repetition
+but a tuple is immutable, unlike lists which are mutable.
+
+
+# s = "Anuj"
+# s[0] = 'C'
+
+a = ('A', 'n', 'u', 'j')
+a[0] = 'c' #not supported
+
+
+print(a)
+
+
+#ex-2
+var = ("Geeks", "for", "Geeks")
+
+print("Value in Var[0] = ", var[0])
+print("Value in Var[1] = ", var[1])
+print("Value in Var[2] = ", var[2])
