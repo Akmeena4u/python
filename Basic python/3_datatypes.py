346f72141e31c78c1ecd017969ac4495d3983e7b
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-30 13:03:26 +0530

    Update 3_datatypes.py

--- Basic python/3_datatypes.py
+++ Basic python/3_datatypes.py
@@ -8,21 +8,30 @@ favorite_food = ['Kiwi', 'Pizza']
 
 
 
-rating = 4.7
+rating = 4.7  #float
 print(rating, type(rating))
 
-name = "Anuj"
+name = "Anuj"  #string
 print(name, type(name))
 
-marks = 98
+marks = 98  #int
 print(marks, type(marks))
 
-is_passed = True
+is_passed = True   #bool
 print(is_passed, type(is_passed))
 
-my_complex_number = 4 + 5j
+my_complex_number = 4 + 5j   #complexnumber
 print(my_complex_number, type(my_complex_number))
 
-fruits_list = ['Apple', 'Banana', 'Kiwi']
+fruits_list = ['Apple', 'Banana', 'Kiwi']  #list
 print(fruits_list, type(fruits_list))
 
+x = ("apple", "banana", "cherry")  #tuple
+print(x,type(x))
+
+
+y = {"name" : "John", "age" : 36}  #	dict -key value pair
+print(y,type(y))
+      
+z = {"apple", "banana", "cherry"}	#set 
+print(z,type(z))
