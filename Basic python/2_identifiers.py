f1ac68c876a1e1f799b73961659ba06d03bdf65d
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-30 12:18:08 +0530

    Update 2_identifiers.py

--- Basic python/2_identifiers.py
+++ Basic python/2_identifiers.py
@@ -1,3 +1,15 @@
+#Comments starts with a #, and Python will ignore them:
+#Python does not really have a syntax for multi line comments.
+#Since Python will ignore string literals that are not assigned to a variable,
+#you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
 #valid identifiers
 age = 24
 name = "Amit"
