36631e997abf5963dc81926bb1961db1b4cc1a5f
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-30 12:10:21 +0530

    Update 1_hello.py

--- Basic python/1_hello.py
+++ Basic python/1_hello.py
@@ -1,1 +1,6 @@
+#Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+#It is used for: web development (server-side),software development,mathematics,system scripting,predictive analysis-ai /ml
+#in python no need to complete a statement by semicolon , here focous on block indentation for loops , conditional statements and functions 
+
+
 print("hello world")
